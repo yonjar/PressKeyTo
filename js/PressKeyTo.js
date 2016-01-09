e4db3573fc29298bf61db86a0e56dca7372a4cb2
Cytrs <1332934648@qq.com>
2016-01-09 13:01:38 +0800

    Update PressKeyTo.js

--- js/PressKeyTo.js
+++ js/PressKeyTo.js
@@ -1,17 +1,17 @@
-	function keyDown(e) {
-        var scrollBottom = $(document).height();
-	    var keycode = e.which;
-	    // var realkey = String.fromCharCode(e.which);
-	    // alert("按键码: " + keycode + " 字符: " + realkey);
-	    if(keycode==17){
-	        $('body,html').animate({
-				scrollTop: 0
-			},1500);
-	    }
-        if(keycode==220){
-            $('body,html').animate({
-    			scrollTop: scrollBottom
-    		},1500);
-        }
-	}
-	document.onkeydown = keyDown;
+function keyDown(e) {
+    var scrollBottom = $(document).height();
+    var keycode = e.which;
+    // var realkey = String.fromCharCode(e.which);
+    // alert("按键码: " + keycode + " 字符: " + realkey);
+    if (keycode == 17) {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 1500);
+    }
+    if (keycode == 220) {
+        $('body,html').animate({
+            scrollTop: scrollBottom
+        }, 1500);
+    }
+}
+document.onkeydown = keyDown;
